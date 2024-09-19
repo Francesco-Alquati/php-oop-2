f31fc1d4306f530a693935690cc547d4a6054d5e
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-09-19 15:36:52 +0200

    [UPD] Created class Category

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php 
-    class Product {
-        public $id;
-        public $name;
-        public $description;
-        public $price;
-        public $category; 
+
     
-        function __construct($_id, $_name, $_description, $_price, $_category) {
-            $this->id = $_id;
-            $this->name = $_name;
-            $this->description = $_description;
-            $this->price = $_price;
-            $this->category = $_category;
-        }
-    
-        public function getInfo() {
-            return "Nome: {$this->name}, Prezzo: {$this->price}";
-        }
-        
-    }
 ?>
---
+++ models/Product.php
@@ -0,0 +1,24 @@
+<?php 
+    class Product {
+        public $name;
+        public $quantity;
+        public $price;
+        public $image;
+        public $category; 
+        public $is_available;
+    
+        function __construct( $_name, $_quantity, $_price, $_image, Category $_category, $_is_available) {
+            $this->name = $_name;
+            $this->quantity = $_quantity;
+            $this->price = $_price;
+            $this->image = $_image;
+            $this->category = $_category;
+            $this->is_available = $_is_available;
+        }
+    
+        public function getProductInfo() {
+            return "Nome: ".$this->name.", Prezzo:".$this->price;
+        }
+    }
+    
+?>
